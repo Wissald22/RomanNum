be76a4a204fe8cde0e8e7fe414d7809258ed5d7e
Wissald22 <wissaldahibi22@gmail.com>
2024-12-19 18:48:41 +0100

    test passed 1

--- src/main/java/com/pantxi/RomaNumerals/RomanNum.java
+++ src/main/java/com/pantxi/RomaNumerals/RomanNum.java
@@ -2,6 +2,7 @@ package com.pantxi.RomaNumerals;
 
 public class RomanNum {
     public static String convert(int nbr ){
-        return "NULL";
+       if (nbr==1) return "I";
+       return "";
     }
 }
