2fb68327daa3aa3e13609edc273ec314b080788e
Wissald22 <wissaldahibi22@gmail.com>
2024-12-19 19:11:19 +0100

    test passing for 10

--- src/main/java/com/pantxi/RomaNumerals/RomanNum.java
+++ src/main/java/com/pantxi/RomaNumerals/RomanNum.java
@@ -2,8 +2,21 @@ package com.pantxi.RomaNumerals;
 
 public class RomanNum {
     public static String convert(int nbr ){
-       //traitement des unités
+        if (nbr <= 0 || nbr > 50)
+            return "Nombre doit etre entre 1 et 50";
+
+
         StringBuilder r= new StringBuilder();
+        // traitement des dizaines
+        switch (nbr / 10) {
+            case 1: r.append("X"); break;
+            case 2: r.append("XX"); break;
+            case 3: r.append("XXX"); break;
+            case 4: r.append("XL"); break;
+            case 5: r.append("L"); break;
+        }
+        nbr =nbr% 10; // pour ecraser les dizaines et laisser les unités
+//traitement des unités
         switch (nbr) {
             case 9: r.append("IX"); break;
             case 8: r.append("VIII"); break;
